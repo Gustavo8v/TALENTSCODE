48991379e1a19ff4b03c229bba40d6bdd4830f73
Tavo <oliver.sykes.van@icloud.com>
2021-09-22 12:13:52 -0500

    Project initialization

---
+++ EjecucionNomina/Modules/Home/View/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  EjecucionNomina
+//
+//  Created by Gustavo on 22/09/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+}
